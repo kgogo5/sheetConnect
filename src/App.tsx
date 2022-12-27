ce4817c64b6a29912c49c8e4ccd7c360a2081bc0
Steve <kgogo9@gmail.com>
2022-12-27 16:16:07 +0900

    실제 구글 스프레드 시트 연동 테스트 성공 - sheet.best api 를 이용한 테스트 성공 하지만 무료는 100회까지만... 추가 결제는 고려 해야함.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,96 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const { register, handleSubmit, control, reset } = useForm();
+
+  const onSubmit = handleSubmit(async (item: any) => {
+    const data = {
+      email: item.email,
+      yesorno: item.yesorno,
+      first: item.first ? "Y" : "N",
+      secound: item.secound ? "Y" : "N",
+      third: item.third ? "Y" : "N",
+      content: item.content || "",
+    };
+
+    await axios
+      .post(
+        "https://sheet.best/api/sheets/5869f7bc-8ec2-4ba1-9bc1-fd7b691ba023",
+        data
+      )
+      .then((response) => {
+        console.log(response);
+        reset();
+      });
+  });
+
+  return (
+    <Box component="form" noValidate autoComplete="off" onSubmit={onSubmit}>
+      <TextField
+        size="small"
+        label="이메일 *"
+        placeholder="이메일 주소를 입력해 주세요."
+        {...register("email")}
+      />
+
+      <Controller
+        name="yesorno"
+        control={control}
+        render={({ field }) => (
+          <RadioGroup {...field}>
+            <FormControlLabel
+              value="Y"
+              control={<Radio />}
+              label={<Box>예</Box>}
+            />
+            <FormControlLabel
+              value="N"
+              control={<Radio />}
+              label={<Box>아니요</Box>}
+            />
+          </RadioGroup>
+        )}
+      />
+
+      <FormControlLabel
+        {...register("first")}
+        control={<Checkbox />}
+        label={<Box>첫번째 선택지</Box>}
+      />
+
+      <FormControlLabel
+        {...register("secound")}
+        control={<Checkbox />}
+        label={<Box>두번째 선택지</Box>}
+      />
+
+      <FormControlLabel
+        {...register("third")}
+        control={<Checkbox />}
+        label={<Box>세번째 선택지</Box>}
+      />
+
+      <Box>
+        <TextField size="small" label="기타사항" {...register("content")} />
+      </Box>
+
+      <Box>
+        <Button color="primary" type="submit">
+          전송
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
